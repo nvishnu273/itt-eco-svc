db1ee4c26fed8dfe8daeba69a9b4d01dfd3b14cf
Navin V Ittiyamparampath <navin.v.i@assyst-coc.srv>
2024-03-25 13:01:47 +0530

    Database connectivity setting...

--- itt_eco_svc_proj/settings.py
+++ itt_eco_svc_proj/settings.py
@@ -74,13 +74,23 @@ WSGI_APPLICATION = "itt_eco_svc_proj.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
+DATABASES = {
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'itt_eco_svc_dev',
+       'USER': '<database_username>',
+       'PASSWORD': '<password>',
+       'HOST': '<database_hostname_or_ip>',
+       'PORT': '<database_port>',
+   }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
